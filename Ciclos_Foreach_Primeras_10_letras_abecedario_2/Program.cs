bc9f3d8dc00a8295a8e0a810859f498460b2d324
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:03:40 -0600

    Agregar archivos de proyecto.

---
+++ Ciclos_Foreach_Primeras_10_letras_abecedario_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+String nombre = "ABCDEFGHIJ";
+foreach (char letra in nombre)
+{
+    Console.Write(" " + letra + " ");
+}
+Console.ReadKey(true);
